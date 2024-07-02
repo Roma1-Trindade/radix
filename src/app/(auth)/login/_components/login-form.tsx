03ad25f50b0174a7909239beac0789b7101ee607
Roma1-Trindade <romaotrindade@hotmail.com>
2024-07-01 23:49:08 -0300

    remove forgot password

--- src/app/(auth)/login/_components/login-form.tsx
+++ src/app/(auth)/login/_components/login-form.tsx
@@ -55,14 +55,6 @@ export default function LoginForm() {
               >
                 Password
               </label>
-              <div className="text-sm">
-                <a
-                  href="#"
-                  className="font-semibold text-indigo-600 hover:text-indigo-500"
-                >
-                  Forgot password?
-                </a>
-              </div>
             </div>
             <div className="mt-2">
               {state?.errors?.password ? (
